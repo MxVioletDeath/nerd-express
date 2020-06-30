126aef1b2acd9af17f152b77df0932b2a946b21e
Violet Death <ladymariaownsbro@gmail.com>
2020-06-29 20:01:27 -0500

    tutor stuff
    shit fucked up though

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name, devoured) VALUES ('Meeses', false);
+INSERT INTO burgers (name, devoured) VALUES ('Bobbi', false);
+INSERT INTO burgers (name, devoured) VALUES ('Sylvester', true);
+INSERT INTO burgers (name, devoured) VALUES ('Marilyn Manson', true);
+INSERT INTO burgers (name, devoured) VALUES ('Joe Biden', true);
+INSERT INTO burgers (name, devoured) VALUES ('Owens', false);
